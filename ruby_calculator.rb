2cda6c084eb524b7abcb4f02f75ca4a21c3221b7
Kingston <jenorish@gmail.com>
2025-02-22 12:41:05 +0530

    DEFAULT_DELIMITERS added and parsed and did sum

--- ruby_calculator.rb
+++ ruby_calculator.rb
@@ -1,9 +1,16 @@
 class RubyCalculator
+  DEFAULT_DELIMITERS = /\n/.freeze
   def self.add(numbers)
     return 0 if numbers.empty?
-
-    nums = numbers.split(",").map(&:to_i)
+    # Removing delimiter
+    nums = parse_numbers(numbers)
     nums.sum
   end
+
+  # Parses the numbers using the given delimiter and converts them to integers
+  def self.parse_numbers(numbers)
+   numbers.gsub(DEFAULT_DELIMITERS, ",").split(",").map(&:to_i)
+  end
+
 end
 
--- spec/ruby_calculator_spec.rb
+++ spec/ruby_calculator_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe RubyCalculator do
     it 'returns 6 for "1,2,3" String' do
       expect(RubyCalculator.add("1,2,3")).to eq(6)
     end
+
+    it 'returns 6 with given "1\n2,3" String' do
+      expect(RubyCalculator.add("1,2,3")).to eq(6)
+    end
   end
 end
